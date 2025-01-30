1c09da7874c0b61c3aadbe52fcc46fdafbedb168
kalyan <kalya@gmail.com>
2025-01-31 02:09:21 +0530

    database connection

--- register.js
+++ register.js
@@ -1,5 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyBb5nCWOKgu4B5UuJWLPYmYqPC2U0NwMfk",
     authDomain: "cars-registerations.firebaseapp.com",
     projectId: "cars-registerations",
@@ -9,13 +15,18 @@ const firebaseConfig = {
     measurementId: "G-WW993G18CG"
   };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
+// Function to Register User
 function registerUser(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); // Prevent form from refreshing
 
     const fullname = document.getElementById("fullname").value;
     const email = document.getElementById("email").value;
