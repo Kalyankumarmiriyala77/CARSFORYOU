f975777d5e8166ad371138c88cbbc7e95e957296
kalyan <kalya@gmail.com>
2025-01-31 02:18:17 +0530

    database connection

--- register.js
+++ register.js
@@ -1,23 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyBb5nCWOKgu4B5UuJWLPYmYqPC2U0NwMfk",
     authDomain: "cars-registerations.firebaseapp.com",
     projectId: "cars-registerations",
     storageBucket: "cars-registerations.firebasestorage.app",
     messagingSenderId: "757300130552",
     appId: "1:757300130552:web:c2dcbb0e459620391ac10d",
-    measurementId: "G-WW993G18CG"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
